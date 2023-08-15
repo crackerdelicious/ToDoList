7d91706409579dc4fd111b58c88b1d4dbb2a4436
Cracker Delicious <crackerdelicious@gmail.com>
2023-08-15 19:53:56 +0700

    New file

--- Program.cs
+++ Program.cs
@@ -2,27 +2,6 @@
 
 namespace ToDoList
 {
-    public class Task
-    {
-        private static int _id = 1;
-        public Task(string title, string description)
-        {
-            TaskId = _id++;
-            TaskTitle = title;
-            TaskDescription = description;
-            TaskCompleted = false;
-        }
-        public int TaskId { get; private set; }
-        public string? TaskTitle { get; private set; }
-        public string? TaskDescription { get; private set; }
-        public bool TaskCompleted { get; set; }
-
-        public override string ToString()
-        {
-            char marked = TaskCompleted ? 'x' : ' ';
-            return $"{TaskId}. [{marked}] {TaskTitle} - {TaskDescription}";
-        }
-    }
     class Program
     {
         static void Main(string[] args)
