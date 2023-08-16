9828e69b6e5a17bd84fc5b6f223692180fa8b71d
Cracker Delicious <crackerdelicious@gmail.com>
2023-08-16 15:38:11 +0700

    Refactoting Codes

--- Program.cs
+++ Program.cs
@@ -4,125 +4,125 @@ namespace ToDoList
 {
     class Program
     {
+        static List<Task> tasks = new List<Task>
+        {
+            new Task("Walking Dogs", "Fisby, Fodo, Lego"),
+            new Task("Buy Groceries", "Fish, Milk, Pork, Chicken"),
+            new Task("Playing with Kids", "Anya, Lorel"),
+            new Task("Learn Programming", "C#, Python, JavaScripts"),
+            new Task("Go for a walk", "From home to park"),
+            new Task("Writing Diary", "List what have done, and plan tomorrow.")
+        };
+
         static void Main(string[] args)
         {
-            List<Task> tasks = new List<Task>
-            {
-                new Task("Walking Dogs", "Fisby, Fodo, Lego"),
-                new Task("Buy Groceries", "Fish, Milk, Pork, Chicken"),
-                new Task("Playing with Kids", "Anya, Lorel"),
-                new Task("Learn Programming", "C#, Python, JavaScripts"),
-                new Task("Go for a walk", "From home to park"),
-                new Task("Writing Diary", "List what have done, and plan tomorrow.")
-            };
-
             int choice;
             do
             {
-                Console.WriteLine("\nTo-Do List Application\n");
-                Console.WriteLine("1. Add a new task");
-                Console.WriteLine("2. View tasks");
-                Console.WriteLine("3. Mark a task as completed");
-                Console.WriteLine("4. Quit");
-                Console.Write("\nEnter your choice: ");
+                PrintMenu();
 
                 if (int.TryParse(Console.ReadLine(), out choice))
                 {
-                    switch (choice)
-                    {
-                        case 1:
-                            AddTask(tasks);
-                            break;
-                        case 2:
-                            ViewTasks(tasks);
-                            break;
-                        case 3:
-                            MarkAsCompleted(tasks);
-                            break;
-                        case 4:
-                            Console.WriteLine("Goodbye!");
-                            break;
-                        default:
-                            Console.WriteLine("You did not select any choices we have.\nPlease select a choice 1 - 4.");
-                            break;
-                    }
+                    HandleChoice(choice);
                 }
                 else
                 {
                     Console.WriteLine("Invalid input. Please enter a numeric value.\n");
                 }
+
             } while (choice != 4);
         }
 
-        static void AddTask(List<Task> tasks)
+        static void PrintMenu()
         {
-            bool result = false;
-            do
-            {
-                Console.Write("Enter task title: ");
-                string? taskTitle = Console.ReadLine();
+            Console.WriteLine("\nTo-Do List Application\n");
+            Console.WriteLine("1. Add a new task");
+            Console.WriteLine("2. View tasks");
+            Console.WriteLine("3. Mark a task as completed");
+            Console.WriteLine("4. Quit");
+            Console.Write("\nEnter your choice: ");
+        }
 
-                if (string.IsNullOrEmpty(taskTitle) || string.IsNullOrWhiteSpace(taskTitle))
+        static void HandleChoice(int choice)
+        {
+            switch (choice)
+            {
+                case 1:
+                    AddNewTask();
+                    break;
+                case 2:
+                    ViewTasks();
+                    break;
+                case 3:
+                    MarkTaskCompleted();
+                    break;
+                case 4:
+                    Console.WriteLine("Goodbye!");
+                    break;
+                default:
+                    Console.WriteLine("You did not select any choices we have.\nPlease select a choice 1 - 4.");
+                    break;
+            }
+        }
+
+        static void AddNewTask()
+        {
+            Console.Write("Enter task title: ");
+            string taskTitle = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(taskTitle) || string.IsNullOrWhiteSpace(taskTitle))
+            {
+                Console.WriteLine("Task title can't be null. Please provide a task title.\n");
+                return;
+            }
+
+            Console.Write("Enter task description: ");
+            string taskDescription = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(taskDescription) || string.IsNullOrWhiteSpace(taskDescription))
+            {
+                taskDescription = "No description for this task.";
+            }
+
+            tasks.Add(new Task(taskTitle, taskDescription));
+            Console.WriteLine($"\nTask \"{taskTitle}\" added.\n");
+        }
+
+        static void ViewTasks()
+        {
+            if (tasks.Count == 0)
+            {
+                Console.WriteLine("\nThere's no task assigned yet.\n");
+                return;
+            }
+
+            Console.WriteLine("\nTasks:");
+            foreach (Task task in tasks)
+            {
+                Console.WriteLine(task);
+            }
+        }
+
+        static void MarkTaskCompleted()
+        {
+            ViewTasks();
+            Console.Write("\nEnter the index of the task to mark as completed: ");
+            if (int.TryParse(Console.ReadLine(), out int taskId))
+            {
+                Task? targetTask = tasks.Find(task => task.TaskId == taskId);
+                if (targetTask != null)
                 {
-                    Console.WriteLine("Task title can't be null. Please provide a task title.\n");
+                    targetTask.TaskCompleted = true;
+                    Console.WriteLine("Task marked as completed.\n");
                 }
                 else
                 {
-                    Console.Write("Enter task description: ");
-                    string? taskDescription = Console.ReadLine();
-                    if (string.IsNullOrEmpty(taskDescription) || string.IsNullOrWhiteSpace(taskDescription))
-                        taskDescription = "No description for this task.";
-
-                    tasks.Add(new Task(taskTitle, taskDescription));
-                    Console.WriteLine($"\nTask \"{taskTitle}\" added.\n");
-                    result = true;
-                }
-            } while (!result);
-        }
-
-        static void ViewTasks(List<Task> tasks)
-        {
-            if (tasks.Count != 0)
-            {
-                Console.WriteLine("\nTasks:");
-                foreach (Task task in tasks)
-                {
-                    Console.WriteLine(task);
+                    Console.WriteLine("No task with the specified ID was found.\n");
                 }
             }
             else
             {
-                Console.WriteLine("\nThere's no task assigned yet.\n");
-            }
-        }
-
-        static void MarkAsCompleted(List<Task> tasks)
-        {
-            if (tasks.Count != 0)
-            {
-                foreach (Task task in tasks)
-                {
-                    Console.WriteLine(task);
-                }
-                bool marked = false;
-                do
-                {
-                    Console.Write("\nEnter the index of the task to mark as completed: ");
-                    if (int.TryParse(Console.ReadLine(), out int taskId))
-                    {
-                        Task? targetTask = tasks.Find(task => task.TaskId == taskId);
-                        if (targetTask != null) targetTask.TaskCompleted = true;
-                        marked = true;
-                    }
-                    else
-                    {
-                        Console.WriteLine("\nNo tasks were found.\n");
-                    }
-                } while (!marked);
-            }
-            else
-            {
-                Console.WriteLine("\nThere's no task assigned yet.\n");
+                Console.WriteLine("Invalid input. Please enter a numeric value.\n");
             }
         }
     }
